d5719ee4d23c9968699f31b0fcf9f36df6da4da6
necip <43618473+ncpy@users.noreply.github.com>
2022-10-14 20:07:53 +0300

    third

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const cors = require('cors');   // frontend to backend
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const product = require("./routes/product")
+const user = require("./routes/user")
 
 const app = express()
 app.use(express.json())
@@ -6,6 +13,14 @@ app.use(express.json())
 app.get("/", (req,res) => {
     res.send("hello world 2")
 })
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from RAsoft pys!" });
+});
+
+
+app.use("/product", product)
+app.use("/user", user)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("server")
