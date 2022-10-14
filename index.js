d7e81a68d4382ce0e59651e9b7cb36644b528e9a
necip <43618473+ncpy@users.noreply.github.com>
2022-10-14 20:04:50 +0300

    second3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get("/", (req,res) => {
-    res.send("hello world")
+    res.send("hello world 2")
 })
 
 app.listen(process.env.PORT || 5000, () => {
